07406d08f04c59d2939ef3b89589a54cd91b977e
Reinaldo Assis <reinaldoams@gmail.com>
2026-06-30 23:10:55 -0300

    fix: NoteFinder Bass click

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,31 @@ import tailwindcss from '@tailwindcss/vite';
 
 import cloudflare from '@astrojs/cloudflare';
 
+/** @see https://docs.astro.build/en/guides/integrations-guide/cloudflare/ */
+function optimizeServerDeps() {
+  return {
+    name: 'optimize-server-deps',
+    /** @param {string} name */
+    configEnvironment(name) {
+      if (name !== 'client') {
+        return {
+          optimizeDeps: {
+            include: ['solid-js', 'solid-js/web'],
+            exclude: ['astro:i18n', 'astro/virtual-modules/i18n.js'],
+          },
+        };
+      }
+    },
+  };
+}
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
 
   vite: {
-      plugins: [tailwindcss()],
-	},
+    plugins: [tailwindcss(), optimizeServerDeps()],
+  },
 
   adapter: cloudflare(),
 
